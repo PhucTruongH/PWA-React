5568c5e41a93e4710191a9e90ae2aff6a2dc4950
PhucTruongH <phuctruong.hanh86@gmail.com>
2023-05-08 16:08:30 +0700

    Convert to PWA

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
+import * as serviceWorkerRegistration from "serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 
@@ -28,3 +29,5 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+serviceWorkerRegistration.register();
