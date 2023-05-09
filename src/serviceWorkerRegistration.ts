80131d3240c3f246960d3b5512694cf0f1d91ed7
PhucTruongH <phuctruong.hanh86@gmail.com>
2023-05-09 13:20:43 +0700

    update logging codes

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -12,10 +12,10 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.0/8 are considered localhost for IPv4.
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 type Config = {
@@ -24,7 +24,6 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  console.log("register");
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(`${process.env.PUBLIC_URL}`, window.location.href);
@@ -48,7 +47,7 @@ export function register(config?: Config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://cra.link/PWA'
+            'worker. To learn more, visit https://cra.link/PWA'
           );
         });
       } else {
@@ -67,6 +66,7 @@ function registerValidSW(swUrl: string, config?: Config) {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
+          console.log("null installingWorker");
           return;
         }
         installingWorker.onstatechange = () => {
@@ -77,11 +77,12 @@ function registerValidSW(swUrl: string, config?: Config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
+                'tabs for this page are closed. See https://cra.link/PWA.'
               );
 
               // Execute callback
               if (config && config.onUpdate) {
+                console.log("PWA onUpdate");
                 config.onUpdate(registration);
               }
             } else {
